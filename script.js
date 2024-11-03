feff37f3b1a50acd92d3d9d69c9d69d78e2d0830
Parksaeroyi1 <142122264+Parksaeroyi1@users.noreply.github.com>
2024-11-03 00:19:41 -0400

    added  json

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const images = ['images/russell.jpg','images/homers.png', 'images/es.png'];
 
+const randomImage = images[Math.floor(Math.random() * images.length)]; // this don't need an arrow function
+const randomProjectImg = document.querySelector(".random_image");  // query image element
+randomProjectImg.src = randomImage // setting src image value to randomImage
 
-const image = () => {
-    return images[Math.floor(Math.random() * images.length)];
-};
+// 
 
-document.getElementById("randomProjectImage");
+const jsonAbout = ` { 
+    "title": "Elijah Suyat",
+    "about": "I’m currently on my journey to becoming a software developer. Passionate about coding and eager to learn, I am open to new job opportunities where I can grow and contribute to exciting projects.",
+    "cover_image": "images/me.jpg"
+} `
+
+const parsedAbout = JSON.parse(jsonAbout);
+
+const displayAbout = () => {
+    const aboutSection = document.querySelector('.about_me_content');
+
+    const profileImage = aboutSection.querySelector('.profile_image');
+    profileImage.src = parsedAbout.cover_image;
+    profileImage.alt = `Cover image of ${parsedAbout.title}`;
+
+    const textAbout = aboutSection.querySelector('.text_about p');
+    textAbout.innerHTML = parsedAbout.about;
+    
+}
+
+displayAbout();
