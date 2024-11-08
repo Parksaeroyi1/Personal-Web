341f09c08d752b3879300a72fcf9401e2aad4e55
ElijahS <142122264+Parksaeroyi1@users.noreply.github.com>
2024-11-07 22:43:51 -0500

    Changes

--- hamburger.js
+++
@@ -1,7 +0,0 @@
- const hamburger = document.getElementById('hamburger');
-    const navLinks = document.querySelector('.header_nav');
-
-    hamburger.addEventListener('click', () => {
-        // hamburger.classList.toggle('show');
-        navLinks.classList.toggle('show');
-    });
